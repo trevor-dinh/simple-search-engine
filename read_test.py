dfdbc8fe4d66052b12c14f98a6665642f034f93e
Trevor <whysoviet@gmail.com>
2018-05-16 17:35:53 -0700

    Added StackOverflow sources to code snippets from read_test.py which will be useful in querying later on

--- read_test.py
+++ read_test.py
@@ -8,6 +8,9 @@ if __name__ == '__main__':
     db = client.invertedIndexDB
     collection = db.indices
     cursor = collection.find({})
-    for document in cursor:
+    #Source: https://stackoverflow.com/a/34598654
+    for document in cursor:  
         pprint(document)
+    print(collection.find_one({"token" : "time"})) 
+    #Source: http://api.mongodb.com/python/current/tutorial.html#getting-a-single-document-with-find-one
 

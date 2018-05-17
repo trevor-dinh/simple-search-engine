47ae8d527ddc1997521f49efd308e59eb3f13fa1
James Purpura <>
2018-05-16 20:49:49 -0700

    Script prints time to invert index

--- script.py
+++ script.py
@@ -3,21 +3,26 @@ from reduce_index import ReduceIndex
 from Document.get_document import read_json, make_document
 import os
 from posting import Posting
-
+from time import time
 
 if __name__ == "__main__":
+    start = time()
     bookkeeping_path = os.path.join(os.getcwd(), 'WEBPAGES_RAW', "bookkeeping.json")
     docs = make_document(read_json(bookkeeping_path), 10)
-    print("(1 / 3) Made Documents")
+    print("(1 / 4) Made Documents")
     td_list = [TokenizeDocument(doc) for doc in docs]
     for td in td_list:
         print(td)
         td.parse()
-    print("(2 / 3) Tokenized Documents")
+    print("(2 / 4) Tokenized Documents")
     red_index = ReduceIndex(td_list)
     red_index.reduce()
-    print("(3 / 3) Reduced Index\n")
+    print("(3 / 4) Reduced Index\n")
+    red_index.calc_tf_idf()
+    print("(4 / 4) TF IDF Calculated")
     print(red_index.reduced_terms)
+    end = time()
+    print("This process took {} seconds".format(round(end - start, 3)))
     # td1 = TokenizeDocument(sample_path1)
     # td2 = TokenizeDocument(sample_path2)
     #

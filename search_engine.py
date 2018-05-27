462cd7ab5cfc1bb1dd4c2247155fe93deb571cd3
Trevor <whysoviet@gmail.com>
2018-05-26 22:07:58 -0700

    Added files for Flask WebApp; changed handle_query to handle flask_pymongo connections as well as general pymongo_connections

---
+++ search_engine.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+from flask import Flask, render_template, request
+import os
+from flask_pymongo import PyMongo
+from Document.get_document import read_json
+from handle_query import Query
+
+BOOKKEEPING_PATH = os.path.join(
+    os.getcwd(), 'WEBPAGES_RAW', "bookkeeping.json")
+LOOKUP = read_json(BOOKKEEPING_PATH)
+DEBUG = True #FOR PRINTING
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["MONGO_HOST"] = "***REMOVED***" #"127.0.0.1"
+app.config["MONGO_DBNAME"] = "invertedIndex"
+app.config["MONGO_USERNAME"] = "***REMOVED***"
+app.config["MONGO_PASSWORD"] = "***REMOVED***"
+app.config["MONGO_CONNECT"] = True
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+#https://stackoverflow.com/questions/12277933/send-data-from-a-textbox-into-flask
+@app.route('/', methods=['GET', 'POST'])
+def query_post():
+    if DEBUG:
+        print(request.method)
+        print(request.form['queryField'])
+    # print('This error output', file=sys.stderr)
+    # print('This standard output', file=sys.stdout)
+    print(request.form['queryField'])
+    if len(request.form['queryField']) == 0 and DEBUG:
+        print("EMPTY!!!")
+    if request.form['queryField'] is not None:
+        search_query = request.form['queryField'].lower()
+        query_results = Query(search_query, mongo.db).handle()
+        if DEBUG:
+            print(query_results)
+        if query_results is not None:
+            list_of_urls = [LOOKUP[doc_id] for doc_id in query_results]
+            return render_template('index.html', 
+                query=request.form['queryField'], posts=list_of_urls)
+        else:
+            return render_template('index.html', noResult=search_query)
+
+
+
+
+if __name__ == "__main__":
+    app.run()
